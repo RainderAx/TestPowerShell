be889a8ba3ba6576ca6d2f1bc2256521916e5364
RainderAx2 <rainderkc@gmail.com>
2024-06-28 13:35:35 +0000

    Maj

--- scriptdoss/doc_shell.ps1
+++ scriptdoss/doc_shell.ps1
@@ -1,6 +1,22 @@
+
+#recuperer la liste des users 
 Get-ADUser -Filter * -SearchBase "CN=Users,DC=cabinet,DC=local" -Server LDAP1.cabinet.local
 
 #connaitre le nom de domaine 
 $domaine = Get-ADDomainController -Discover
 
+#print 
 Write-Output "Nom du Serveur AD : $($domaine.HostName)"
+
+# Créer un fichier
+$gojo = "nomdufichier"
+New-Item -Path "C:\Scripts\Alexis\$gojo.txt" -ItemType File -Force
+
+# Convertir les données utilisateur en chaîne de caractères
+$userData = $satoru | ForEach-Object { "$($_.Name), $($_.SamAccountName)" }
+
+# Cominer des données 
+$donnees = "$userData, $gojo"
+
+# Écrire les données dans le fichier
+$donnees | Out-File -FilePath "C:\Scripts\Alexis\$gojo.txt"
--- scriptdoss/recherche.ps1
+++ scriptdoss/recherche.ps1
@@ -1,15 +1,11 @@
 # Obtenir tous les utilisateurs AD de la base de recherche spécifiée
 $satoru = Get-ADUser -Filter * -SearchBase "CN=Users,DC=cabinet,DC=local" -Server LDAP1.cabinet.local
 
-# Nom du fichier
 $gojo = "nomdufichier"
 
 # Créer le fichier si nécessaire
 New-Item -Path "C:\Scripts\Alexis\$gojo.txt" -ItemType File -Force
 
-
-# Convertir les données utilisateur en chaîne de caractères
-# Ici, on choisit d'utiliser les propriétés Name et SamAccountName comme exemple
 $userData = $satoru | ForEach-Object { "$($_.Name), $($_.SamAccountName)" }
 
 # Préparer les données pour l'écriture dans le fichier
