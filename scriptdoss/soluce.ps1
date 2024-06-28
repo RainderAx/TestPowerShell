9dc3b3134511e01180277518e069f7daa795e56f
Rainder <114318268+RainderAx@users.noreply.github.com>
2024-06-28 13:27:06 +0000

    réponse?

--- scriptdoss/soluce.ps1
+++ scriptdoss/soluce.ps1
@@ -1,6 +1,19 @@
+# Obtenir tous les utilisateurs AD de la base de recherche spécifiée
 $satoru = Get-ADUser -Filter * -SearchBase "CN=Users,DC=cabinet,DC=local" -Server LDAP1.cabinet.local
+
+# Nom du fichier
 $gojo = "nomdufichier"
 
+# Créer le fichier si nécessaire
+New-Item -Path "C:\Scripts\Alexis\$gojo.txt" -ItemType File -Force
 
-$donnees = "$satoru,$gojo"
-$donnees | Out-File "\\C:\Scripts\Alexis\gojo.txt"
+
+# Convertir les données utilisateur en chaîne de caractères
+# Ici, on choisit d'utiliser les propriétés Name et SamAccountName comme exemple
+$userData = $satoru | ForEach-Object { "$($_.Name), $($_.SamAccountName)" }
+
+# Préparer les données pour l'écriture dans le fichier
+$donnees = "$userData, $gojo"
+
+# Écrire les données dans un fichier
+$donnees | Out-File -FilePath "C:\Scripts\Alexis\$gojo.txt"
