7639671d499f1e9550c3a6966a0c87e3cab166a9
RainderAx2 <rainderkc@gmail.com>
2024-07-02 14:52:35 +0000

    pas les droits d acces

--- scriptdoss/recherche.ps1
+++ scriptdoss/recherche.ps1
@@ -1,14 +1,15 @@
+#Set-ADUser : Droits d’accès insuffisants pour effectuer cette opération
+Au caractère Ligne:8 : 1
++ Set-ADUser -Identity $user -Office '$buro'
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : NotSpecified: (alexis.joseph.cab:ADUser) [Set-ADUser], ADException
+    + FullyQualifiedErrorId : ActiveDirectoryServer:8344,Microsoft.ActiveDirectory.Management.Commands.SetADUser
+##
+
 $poire = "alexis.joseph"
 
 $buro = 6013
 
+$user = (Get-ADUser -Filter "UserPrincipalName -like '$poire*'" -Properties *).SamAccountName
 
-$user = Get-ADUser -Filter "samAccountName -eq '$poire'"
-
-
-Set-ADUser -Identity $user -Replace @{physicalDeliveryOfficeName = $buro}
-
-(Get-ADUser -Filter "UserPrincipalName -like '$poire*'" -Properties *).Office
-
-
-
+Set-ADUser -Identity $user -Office '$buro'
