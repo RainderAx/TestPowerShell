c9e95584f22aab0b1b0af28f25173ca7c0991b93
RainderAx2 <rainderkc@gmail.com>
2024-07-02 14:28:25 +0000

    err

--- scriptdoss/recherche.ps1
+++ scriptdoss/recherche.ps1
@@ -1,17 +1,14 @@
-# Spécifiez le samAccountName de l'utilisateur
 $poire = "alexis.joseph"
 
-# Spécifiez le nouveau numéro de bureau
-$buro = "6013"
+$buro = 6013
 
-# Récupérer l'utilisateur pour obtenir l'Identity unique
+
 $user = Get-ADUser -Filter "samAccountName -eq '$poire'"
 
-# Mettre à jour le bureau de l'utilisateur
+
 Set-ADUser -Identity $user -Replace @{physicalDeliveryOfficeName = $buro}
 
-# Vérifier la mise à jour en récupérant à nouveau l'utilisateur
-Get-ADUser -Filter "samAccountName -eq '$poire'" -Properties *
+(Get-ADUser -Filter "UserPrincipalName -like '$poire*'" -Properties *).Office
 
 
 
