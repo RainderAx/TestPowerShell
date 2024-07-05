3f32a9edc011013846301f25b74b4d0ae7f8b896
Rainder <114318268+RainderAx@users.noreply.github.com>
2024-07-05 08:43:41 +0000

    doc

--- scriptdoss/doc_shell.ps1
+++ scriptdoss/doc_shell.ps1
@@ -58,3 +58,14 @@ $comboBox.DropDownStyle = [System.Windows.Forms.ComboBoxStyle]::DropDownList
 # Ajouter des options au menu déroulant
 $options = @("BAT4", "BAT5", "BAT6", "ROQUELAURE", "LEPLAY", "LESDIGUIERE")
 $comboBox.Items.AddRange($options)
+
+##Switch Case
+$selectedOption = $comboBox.SelectedItem
+
+    switch ($selectedOption) {
+        "Option 1" { [System.Windows.Forms.MessageBox]::Show("Vous avez sélectionné : Option 1") }
+        "Option 2" { [System.Windows.Forms.MessageBox]::Show("Vous avez sélectionné : Option 2") }
+        "Option 3" { [System.Windows.Forms.MessageBox]::Show("Vous avez sélectionné : Option 3") }
+        "Option 4" { [System.Windows.Forms.MessageBox]::Show("Vous avez sélectionné : Option 4") }
+        default { [System.Windows.Forms.MessageBox]::Show("Option non reconnue") }
+    }
