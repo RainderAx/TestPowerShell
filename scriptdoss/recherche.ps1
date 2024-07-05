071462ed54d491b7a615060f5c06bc89c9eba980
RainderAx2 <rainderkc@gmail.com>
2024-07-05 09:38:44 +0000

    .

--- scriptdoss/recherche.ps1
+++ scriptdoss/recherche.ps1
@@ -1,3 +1,12 @@
 $valider.Add_Click(
     Write-Output "Nom du Serveur AD : $($domaine.HostName)"
 )
+
+Impossible de convertir l'argument «value» (valeur «OK») de «add_Click» en type «System.EventHandler»: «Impossible de 
+convertir la valeur «OK» en type «System.EventHandler». Erreur: «Cast non valide de 'System.Windows.Forms.DialogResult' en 
+'System.EventHandler'.»»
+Au caractère Ligne:117 : 1
++ $valider.Add_Click(
++ ~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : NotSpecified: (:) [], MethodException
+    + FullyQualifiedErrorId : MethodArgumentConversionInvalidCastArgument
