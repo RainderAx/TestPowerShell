e2d9de4c8a6602c9e98b213c5abdd58772131d24
RainderAx2 <rainderkc@gmail.com>
2024-07-09 09:04:29 +0000

    rec

--- scriptdoss/recherche.ps1
+++ scriptdoss/recherche.ps1
@@ -22,3 +22,21 @@ $window.FindName("Close_Click").Add_Click({
 })
 
 $window.ShowDialog()
+
+$Title = "Continue"
+$Info = "Would you like to continue?"
+ 
+$Options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Yes", "&No")
+[int]$DefaultChoice = 0
+$Opt =  $host.UI.PromptForChoice($Title , $Info, $Options, $DefaultChoice)
+
+switch($Opt)
+{
+	0 { 
+		Write-Verbose -Message "Yes"
+	}
+	
+	1 { 
+		Write-Verbose -Message "No" 
+	}
+}
