75465f5fd155a91ef69c9511218dc6614de0257c
RainderAx2 <rainderkc@gmail.com>
2024-07-12 14:51:07 +0000

    !

--- scriptdoss/recherche.ps1
+++ scriptdoss/recherche.ps1
@@ -16,3 +16,22 @@ if ($users) {
 } else {
     Write-Output "Aucun utilisateur trouvé avec le filtre spécifié."
 }
+
+
+DistinguishedName : CN=JOSEPH Alexis,OU=PSNSM2,OU=PSNSM,OU=DSP,OU=cabinet,DC=cabinet,DC=local
+Enabled           : True
+GivenName         : Alexis
+Name              : JOSEPH Alexis
+ObjectClass       : user
+ObjectGUID        : e5fd88ff-39d4-45d9-a120-861bc92da464
+SamAccountName    : alexis.joseph.cab
+SID               : S-1-5-21-1940544878-2514950346-2276345395-38703
+Surname           : JOSEPH
+UserPrincipalName : alexis.joseph.cab@i-carre.net
+
+Set-ADUser : Droits d’accès insuffisants pour effectuer cette opération
+Au caractère Ligne:14 : 9
++         Set-ADUser -Identity $users.SamAccountName -Office '1234'
++         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : NotSpecified: (alexis.joseph.cab:ADUser) [Set-ADUser], ADException
+    + FullyQualifiedErrorId : ActiveDirectoryServer:8344,Microsoft.ActiveDirectory.Management.Commands.SetADUser
