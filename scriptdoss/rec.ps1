7539f06f133f0db33e8011d322f09a0e04759fad
Rainder <114318268+RainderAx@users.noreply.github.com>
2024-07-24 08:33:18 +0000

    og bg

--- scriptdoss/rec.ps1
+++ scriptdoss/rec.ps1
@@ -1,51 +1,78 @@
+Add-Type -AssemblyName System.Windows.Forms
+
 $prenom = "alexis"
 $nom = "joseph"
 
 # Récupérer l'utilisateur correspondant au prénom et nom fournis
 $user = Get-ADUser -Filter "GivenName -like '$($prenom)*' -and Surname -like '$($nom)*'" -Properties *
 
-# Vérifier si un utilisateur a été trouvé
+# Créer un formulaire
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Informations Utilisateur AD"
+$form.Size = New-Object System.Drawing.Size(400, 300)
+
+# Créer un TabControl
+$tabControl = New-Object System.Windows.Forms.TabControl
+$tabControl.Dock = [System.Windows.Forms.DockStyle]::Fill
+
+# Créer le TabPage pour les informations utilisateur
+$tabPage = New-Object System.Windows.Forms.TabPage
+$tabPage.Text = "Détails Utilisateur"
+
 if ($user) {
-    # Afficher toutes les propriétés de l'utilisateur
-    $Na = $user | Select-Object Name
-    $Mail = $user | Select-Object mail 
-    $AccountExpirationDate = $user | Select-Object AccountExpirationDate
-    $telephoneNumber = $user | Select-Object telephoneNumber
-    $Title = $user | Select-Object Title
-    $log=$user | Select-Object Title SamAccountName
-    
+    # Récupérer les propriétés de l'utilisateur
+    $Na = $user.Name
+    $Mail = $user.mail 
+    $AccountExpirationDate = $user.AccountExpirationDate
+    $telephoneNumber = $user.telephoneNumber
+    $Title = $user.Title
+    $log = $user.SamAccountName
+
+    # Afficher les propriétés dans des labels
+    $OngletBLabelPrenom = New-Object System.Windows.Forms.Label
+    $OngletBLabelPrenom.Location = New-Object System.Drawing.Point(30, 20)
+    $OngletBLabelPrenom.Size = New-Object System.Drawing.Size(300, 20)
+    $OngletBLabelPrenom.Text = "Nom: $Na"
+
+    $OngletBLabelMail = New-Object System.Windows.Forms.Label
+    $OngletBLabelMail.Location = New-Object System.Drawing.Point(30, 50)
+    $OngletBLabelMail.Size = New-Object System.Drawing.Size(300, 20)
+    $OngletBLabelMail.Text = "Email: $Mail"
+
+    $OngletBLabelAccExp = New-Object System.Windows.Forms.Label
+    $OngletBLabelAccExp.Location = New-Object System.Drawing.Point(30, 80)
+    $OngletBLabelAccExp.Size = New-Object System.Drawing.Size(300, 20)
+    $OngletBLabelAccExp.Text = "Expiration du compte: $AccountExpirationDate"
+
+    $OngletBLabelTelephone = New-Object System.Windows.Forms.Label
+    $OngletBLabelTelephone.Location = New-Object System.Drawing.Point(30, 110)
+    $OngletBLabelTelephone.Size = New-Object System.Drawing.Size(300, 20)
+    $OngletBLabelTelephone.Text = "Téléphone: $telephoneNumber"
+
+    $OngletBlalbelLog = New-Object System.Windows.Forms.Label
+    $OngletBlalbelLog.Location = New-Object System.Drawing.Point(30, 140)
+    $OngletBlalbelLog.Size = New-Object System.Drawing.Size(300, 20)
+    $OngletBlalbelLog.Text = "Login: $log"
+
+    # Ajouter les labels au TabPage
+    $tabPage.Controls.Add($OngletBLabelPrenom)
+    $tabPage.Controls.Add($OngletBLabelMail)
+    $tabPage.Controls.Add($OngletBLabelAccExp)
+    $tabPage.Controls.Add($OngletBLabelTelephone)
+    $tabPage.Controls.Add($OngletBlalbelLog)
 } else {
-    Write-Host "Utilisateur non trouvé"
+    $noUserLabel = New-Object System.Windows.Forms.Label
+    $noUserLabel.Location = New-Object System.Drawing.Point(30, 20)
+    $noUserLabel.Size = New-Object System.Drawing.Size(300, 20)
+    $noUserLabel.Text = "Utilisateur non trouvé"
+    $tabPage.Controls.Add($noUserLabel)
 }
 
-$OngletBLabelPrenom = New-Object System.Windows.Forms.Label
-$OngletBLabelPrenom.Location = New-Object System.Drawing.Point(30, 140)
-$OngletBLabelPrenom.Text= $Na
-$OngletBLabelPrenom.Size = New-Object System.Drawing.Size(80, 20)
+# Ajouter le TabPage au TabControl
+$tabControl.TabPages.Add($tabPage)
 
+# Ajouter le TabControl au formulaire
+$form.Controls.Add($tabControl)
 
-$OngletBLabelTelephone = New-Object System.Windows.Forms.Label
-$OngletBlabelTelephone.Location = New-Object System.Drawing.Point(30, 120)
-$OngletBLabelTelephone.Size = New-Object System.Drawing.Size(71, 13)
-$OngletBLabelTelephone.Text = $telephoneNumber
-
-$OngletBlalbelLog = New-Object System.Windows.Forms.Label
-$OngletBlalbelLog.Location = New-Object System.Drawing.Point(70, 150)
-$OngletBlalbelLog.Size = New-Object System.Drawing.Size(71, 13)
-$OngletBlalbelLog.Text=$log
-
-$OngletBLabelMail =  New-Object System.Windows.Forms.Label
-$OngletBLabelMail.Location = New-Object System.Drawing.Point(45, 150)
-$OngletBLabelMail.Size = New-Object System.Drawing.Size(71, 13)
-$OngletBLabelMail.text = $Mail
-
-$OngletBLabelAccExp =  New-Object System.Windows.Forms.Label
-$OngletBLabelAccExp.Location = New-Object System.Drawing.Point(45, 150)
-$OngletBLabelAccExp.Size = New-Object System.Drawing.Size(71, 13)
-$OngletBLabelAccExp.text = $AccountExpirationDate
-
-$TabPage2.Controls.Add($OngletBLabelPrenom)
-$TabPage2.Controls.Add($OngletBLabelTelephone)
-$TabPage2.Controls.Add($OngletBlalbelLog)
-$TabPage2.Controls.Add($OngletBLabelMail)
-$TabPage2.Controls.Add($OngletBLabelAccExp)
+# Afficher le formulaire
+$form.ShowDialog()
