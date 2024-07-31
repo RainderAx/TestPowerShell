d6ecb781b0c5d887c3d740f447b43e2f25d3986f
RainderAx2 <rainderkc@gmail.com>
2024-07-31 10:34:39 +0000

    script2 modif

--- scriptdoss/modif_script2.ps1
+++ scriptdoss/modif_script2.ps1
@@ -1,153 +1,108 @@
-####
-####
-##################################################################################################################################################################
-#############################  Ce Script manipule des fichiers géénérés par autre script (description-ordinateur) lors de l'installation du PC ###################
-
-#############################  Ce Script met à jour le champ description pc ######################################################################################
-
-#############################  Ce script déplace le PC dans la bonne OU ###########################################################################################
-
-#############################  Ce script supprime le pc du groupe "grp-ordinateurs-Stock_Tampon"  #################################################################
-
-####################################################################################################################################################################
-# ce script sera executé avec une tâche planifiée.
 # Spécifiez le chemin du répertoire à traiter
+$chemin = "C:\Scripts\Alexis\test_pour_script2"
 
-$path = "C:\Scripts\Alexis\test_pour_script2 "
 # Obtenez tous les fichiers dans le répertoire
- $Userfiler = Get-ChildItem $path
-# répertoire de destination 
-#variable modifiée
-$emplacement = "C:\Scripts\Alexis\test_pour_script2\Deuxième_Dossier"
-# réinitilaisation de la variable
-#variable modifiée
-$file = $null
-# variable pour ignorer toutes les erreurs et ne pas les afficher
+$Pcfiler = Get-ChildItem $chemin
+
+# Répertoire de destination
+$destination = "C:\Scripts\Alexis\test_pour_script2\archive"
+
+# Initialisation de la variable
+$fichier = $null
+
+# Variable pour ignorer toutes les erreurs et ne pas les afficher
 $ErrorActionPreference= 'silentlycontinue'
-# Pour chaque fichier, affichez le contenu
 
-#variable modifiée
-foreach ($file in $Userfiler) {  
-    Get-Content $path\$file | ForEach-Object {
-            
-        # Séparez le contenu avec le délimiteur ","
+# Fonction pour gérer les ordinateurs
+function Manage-Computer {
+    param(
+        [string]$Computer,
+        [string]$Description_Pc,
+        [string]$Grp_Pc,
+        [string]$Ou_Pc
+    )
 
-        $Nom= $_.Split(",")[0]
-        $Prenom = $_.Split(",")[1]
-        $Bureau = $_.Split(",")[2]
-        $Login = $_.Split(",")[3]
-        $Telephone = $_.Split(",")[4]
+    # Description Ordinateur
+    Set-ADComputer -Identity $Computer -Description $Description_Pc -Confirm:$false
+    
+    # Rajout de l'ordinateur dans le groupe correspondant
+    Add-AdGroupMember -Identity $Grp_Pc -Members $Computer -Confirm:$false
 
-# les variables nécessaires au fonctionnement du script
+    # Suppression de l'ordinateur du "groupe grp-ordinateurs-Stock_Tampon"
+    Remove-ADGroupMember -Identity "grp-ordinateurs-Stock_Tampon" -Members $Computer -Confirm:$false
 
-#variable modifiée
-$Utilis = (Get-ADUser -Filter "UserPrincipalName -like '$Login*'" -Properties *).SamAccountName
-
-$Ou_Pc = "OU=$Cabinet,OU=ordinateurs,OU=infra,DC=cabinet,DC=local"
-
-#
-######################## manipulation de l'objet.
-#
-
-###ajout
-### Modifier le bureau
-Set-ADUser -Identity $Utilis -Office '$Bureau'
-
-
-if ($Cabinet -like "OH") {
-
-    $Users_OH = (Get-ADUser -Identity "$Prenom.$Nom" –Properties * ).department
-
-    $Cab_OH = $Users_OH.split("/")[1]
-
-    $Cab_OH_0 = $Users_OH.split("/")[0]
-
-    $Description_Pc = "$Nom $Prenom Bur: $Bureau Tel: $tel $Cab_OH"
-
-    $Grp_Pc = "grp-ordinateurs-$Cab_OH"
-
-    $Ou_Pc = "OU=$Cab_OH, OU=$Cab_OH_0,OU=OH,OU=ordinateurs,OU=infra,DC=cabinet,DC=local"
-
-
-    Manage-Computer -Computer $Computer  -Description_Pc $Description_Pc -Grp_Pc $Grp_Pc -Ou_Pc $Ou_Pc 
-
+    # Déplacement de l'ordinateur dans la bonne OU correspondante
+    Move-ADObject -Identity $Computer -TargetPath $Ou_Pc -Confirm:$false
 }
-####
 
-#
-# verifier de l'envoie de notification email
-#
+# Pour chaque fichier, affichez le contenu
+foreach ($fichier in $Pcfiler) {
+    $filePath = "$chemin\$fichier"
 
-#ajout
-$second_mail = "administrateurs.psnsm@cabinet.local"
+    if ($fichier.Name -match "P-PC.*\.txt") {
+        # Traitement pour le premier type de fichier (numéro de série du PC suivi du nom de famille)
+        Get-Content $filePath | ForEach-Object {
+            $Nom = $_.Split(",")[0]
+            $Prenom = $_.Split(",")[1]
+            $Bureau = $_.Split(",")[2]
+            $Tel = $_.Split(",")[3]
+            $Cabinet = $_.Split(",")[4]
+            $Pc = $_.Split(",")[5]
 
-$Sujet = "Deplacement $Nom $Prenom au bureau $Bureau"
+            # Variables nécessaires au fonctionnement du script
+            $Computer = (Get-ADComputer -Identity $Pc -Properties *).DistinguishedName
+            $grp_User = "grp-utilisateurs-$Cabinet"
+            $Grp_Pc = "grp-ordinateurs-$Cabinet"
+            $Ou_Pc = "OU=$Cabinet,OU=ordinateurs,OU=infra,DC=cabinet,DC=local"
+            $PcMemberOf = (Get-ADComputer -Identity $Pc -Properties *).memberof
+            $Description_Pc = "$Nom $Prenom Bur: $Bureau Tel: $Tel $Cabinet"
 
-$SmtpServer = ""
-
-$corps = @"
-
-    Bonjour,
-
-    Le bureau de $Nom $Prenom est maintenant en $Bureau         
-
-"@
-#####
-
-if ($Cabinet -like "SETI4") {
-
-            $SET4 = "set4"
-
-            $Grp_Pc = "grp-ordinateurs-set4"
-
-            $Description_Pc = "$Nom $Prenom Bur: $Bureau Tel: $SET4"
-
-            $Ou_Pc ="OU=seti4,OU=ordinateurs,OU=infra,DC=cabinet,DC=local"
-
-
-            Manage-Computer -Computer $Computer  -Description_Pc $Description_Pc -Grp_Pc $Grp_Pc -Ou_Pc $Ou_Pc
-     
+            if ($Cabinet -like "OH") {
+                $Users_OH = (Get-ADUser -Identity "$Prenom.$Nom" –Properties * ).department
+                $Cab_OH = $Users_OH.split("/")[1]
+                $Cab_OH_0 = $Users_OH.split("/")[0]
+                $Description_Pc = "$Nom $Prenom Bur: $Bureau Tel: $Tel $Cab_OH"
+                $Grp_Pc = "grp-ordinateurs-$Cab_OH"
+                $Ou_Pc = "OU=$Cab_OH, OU=$Cab_OH_0,OU=OH,OU=ordinateurs,OU=infra,DC=cabinet,DC=local"
+            } elseif ($Cabinet -like "SETI4") {
+                $SET4 = "set4"
+                $Grp_Pc = "grp-ordinateurs-set4"
+                $Description_Pc = "$Nom $Prenom Bur: $Bureau Tel: $SET4"
+                $Ou_Pc = "OU=seti4,OU=ordinateurs,OU=infra,DC=cabinet,DC=local"
+            } elseif ($Cabinet -match "DSP") {
+                $DSP = "psnsm"
+                $Description_Pc = "$Nom $Prenom Bur: $Bureau Tel: $Tel $DSP"
+                $Grp_Pc = "grp-ordinateurs-psnsm"
+                $Ou_Pc = "OU=psnsm,OU=ordinateurs,OU=infra,DC=cabinet,DC=local"
             }
 
-             elseif ($Cabinet -match "DSP") {
-
-                     $DSP = "psnsm"
-
-                     $Description_Pc = "$Nom $Prenom Bur: $Bureau Tel: $tel $DSP"
-
-                     $Grp_Pc = "grp-ordinateurs-psnsm"
- 
-                     $Ou_Pc ="OU=psnsm,OU=ordinateurs,OU=infra,DC=cabinet,DC=local"
-
-             
-                     Manage-Computer -Computer $Computer  -Description_Pc $Description_Pc -Grp_Pc $Grp_Pc -Ou_Pc $Ou_Pc
-      
-                        } else {
-
-                         Manage-Computer -Computer $Computer  -Description_Pc $Description_Pc -Grp_Pc $Grp_Pc -Ou_Pc $Ou_Pc
-
-
-                        }
- 
-
+            Manage-Computer -Computer $Computer -Description_Pc $Description_Pc -Grp_Pc $Grp_Pc -Ou_Pc $Ou_Pc
         }
+    } elseif ($fichier.Name -match "^\w+\.\w+(\.cab)?\.txt$") {
+        # Traitement pour le deuxième type de fichier (prénom.nom ou prénom.nom.cab)
+        Get-Content $filePath | ForEach-Object {
+            # Exemple de traitement, à adapter selon vos besoins spécifiques
+            $Login = $_.Split(",")[1]           
+            $Bureau = $_.Split(",")[0]
 
-        #
-        ########################### Mail de notification
+            # Variables nécessaires au fonctionnement du script
+            $Computer = (Get-ADComputer -Identity $Pc -Properties *).DistinguishedName
+            $grp_User = "grp-utilisateurs-$Cabinet"
+            $Grp_Pc = "grp-ordinateurs-$Cabinet"
+            $Ou_Pc = "OU=$Cabinet,OU=ordinateurs,OU=infra,DC=cabinet,DC=local"
+            $Description_Pc = "$Prenom $Nom Bur: $Bureau Tel: $Tel $Cabinet"
 
-        Send-MailMessage -From $mail -To $mail -Subject $Sujet  -Body $body -SmtpServer $SmtpServer -port 587 -Encoding unicode
+            
 
-         #
-         ############# une fois la modification est à jour, la vérification du groupe ordinateur et d'OU ordinateur, le fichier serait  deplacé dans le dossier Archives
-         #
+            Set-ADUser -Identity $Login -Office "$Bureau"
+            #test console
+            Write-Host "$Login ,  $Bureau"
+        }
+    }
 
-        Move-Item -Path "$chemin\$fichier"  -Destination $destination
+    # Envoi de l'email de notification
+    Send-MailMessage -From $mail -To $mail -Subject $Subject -Body $body -SmtpServer $SmtpServer -port 587 -Encoding unicode
 
-
-        #ajout
-        Send-MailMessage -From $second_mail -To $second_mail -Subject $Subject  -Body $corps -SmtpServer $SmtpServer -port 587 -Encoding unicode
-
-        ###ajout
-        Move-Item -Path "$path\$file"  -Destination $emplacement
-
- }
+    # Déplacement du fichier traité vers le dossier Archives
+    Move-Item -Path $filePath -Destination $destination
+}
