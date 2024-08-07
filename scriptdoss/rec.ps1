61ae3b684e1bc14f3d7b37870207bf936547b104
RainderAx2 <rainderkc@gmail.com>
2024-08-07 08:48:34 +0000

    pb

--- scriptdoss/rec.ps1
+++ scriptdoss/rec.ps1
@@ -1,4 +1,10 @@
-elseif ($fichier.Name -match "^U.*\.txt")
+$Users_OH = (Get-ADUser -Identity "Sophie.BEAUDOUIN HUBIERE" –Properties * ).SamAccountName
 
-if ($fichier.Name -match "^C.*\.txt")
+Write-Host $Users_OH
 
+Get-ADUser : Impossible de trouver un objet avec l’identité « Sophie.BEAUDOUIN HUBIERE » sous : « DC=cabinet,DC=local ».
+Au caractère Ligne:2 : 14
++ ... Users_OH = (Get-ADUser -Identity "Sophie.BEAUDOUIN HUBIERE" –Properti ...
++                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (Sophie.BEAUDOUIN HUBIERE:ADUser) [Get-ADUser], ADIdentityNotFoundException
+    + FullyQualifiedErrorId : ActiveDirectoryCmdlet:Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException,Microsoft.ActiveDirectory.Management.Commands.GetADUser
